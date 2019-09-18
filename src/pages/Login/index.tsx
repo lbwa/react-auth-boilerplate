c681648bc8a9fcdd2e7696dd5ab6fa9442531e9a
bowen <lbwa@users.noreply.github.com>
2019-09-18 17:06:50 +0800

    feat(login): remove redundant withRouter
    All route exports of react-router will be passed 3 props: match,
    location, history. So we can remove redundant withRouter in Login page
    which plays a router export role.
    DOC: https://reacttraining.com/react-router/core/api/Route/route-props

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState, ChangeEvent } from 'react'
+import React, { useState, ChangeEvent, KeyboardEvent } from 'react'
+import { History } from 'history'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Snackbar from '@material-ui/core/Snackbar'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-import { withRouter } from 'react-router-dom'
 import { Central } from '../../layouts'
 import { userLogin } from '../../apis'
 import { useDispatch } from 'react-redux'
@@ -12,6 +12,10 @@ import { setUser as setUserAction } from '../../store/actions'
 interface LoginInfo {
   name: string
   password: string
+}
+
+interface LoginProps {
+  history: History
 }
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -27,13 +31,19 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 )
 
-function Login() {
+/**
+ * All route exports of react-router will be passed 3 props: match, history, location
+ * @doc https://reacttraining.com/react-router/core/api/Route/route-props
+ */
+function Login({ history }: LoginProps) {
   const classes = useStyles()
+
   const [user, setUser] = useState({
     name: '',
     password: ''
   })
   const [loading, setLoading] = useState(false)
+
   const [snackbarState, setSnackbarState] = useState<{
     open: boolean
     message: string
@@ -41,6 +51,7 @@ function Login() {
     open: false,
     message: ''
   })
+
   const onChangeEvt = (key: keyof LoginInfo) => (
     evt: ChangeEvent<HTMLInputElement>
   ) =>
@@ -49,36 +60,39 @@ function Login() {
       [key]: evt.target.value
     })
 
-  const ToOverview = withRouter(({ history }) => (
-    <Button
-      className={classes.item}
-      variant="contained"
-      color="primary"
-      onClick={() => {
-        if (!user.name || !user.password) return
-        setLoading(true)
-        userLogin(user.name, user.password)
-          .then(({ token }) => {
-            setLoading(false)
-            token && dispatch(setUserAction({ token }))
-            history.replace('/overview')
-          })
-          .catch((err: Error) => {
-            console.error(err.message || err)
-            setSnackbarState({
-              ...snackbarState,
-              open: true,
-              message: 'Incorrect username or password'
-            })
-            setLoading(false)
-          })
-      }}
-    >
-      Login
-    </Button>
-  ))
-
   const dispatch = useDispatch()
+
+  function useLogin(username: string, password: string, shouldLogin: boolean) {
+    if (!shouldLogin) return
+    setLoading(true)
+    userLogin(username, password)
+      .then(({ token }) => {
+        setLoading(false)
+        token && dispatch(setUserAction({ token }))
+        history.replace('/overview')
+      })
+      .catch((err: Error) => {
+        console.error(err.message || err)
+        setSnackbarState({
+          ...snackbarState,
+          open: true,
+          message: 'Incorrect username or password'
+        })
+        setLoading(false)
+      })
+  }
+
+  const useEnter = (evt: KeyboardEvent) => {
+    useLogin(
+      user.name,
+      user.password,
+      !(evt.key !== 'Enter' || !user.name || !user.password)
+    )
+  }
+
+  const useUserLogin = () => {
+    useLogin(user.name, user.password, !(!user.name || !user.password))
+  }
 
   return (
     <Central loading={loading}>
@@ -92,10 +106,18 @@ function Login() {
         className={classes.item}
         value={user.password}
         onChange={onChangeEvt('password')}
+        onKeyPress={useEnter}
         label="Password"
         type="password"
       ></TextField>
-      <ToOverview />
+      <Button
+        className={classes.item}
+        variant="contained"
+        color="primary"
+        onClick={useUserLogin}
+      >
+        Login
+      </Button>
       <Snackbar
         anchorOrigin={{
           vertical: 'bottom',
