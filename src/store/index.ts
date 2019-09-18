e1cd3e4c9ea1c56dafcc6b537dd185ad4406905a
bowen <lbwa@users.noreply.github.com>
2019-09-18 18:49:19 +0800

    feat(store): add __DEV__ variable

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { __DEV__ } from '../shared/env'
 
 declare global {
   interface Window {
@@ -9,5 +10,9 @@ declare global {
 
 export default createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  __DEV__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+export type storeType = ReturnType<typeof reducer>
